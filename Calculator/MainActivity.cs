e5d44392cdb026161b5db333e77f18a199d45387
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-11-03 09:17:49 +0200

    Add project files.

---
+++ Calculator/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using System.Linq;
+
+namespace Calculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@android:style/Theme.DeviceDefault.NoActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public string Number;
+        public string NumberInput;
+
+        public string GetNumberInput(string input)
+        {
+            if (input == "*" ||
+                input == "/" ||
+                input == "-" ||
+                input == "+" ||
+                input == "=" ||
+                input == "DEL") return Number;
+            else return Number = Number + input;
+        }
+        public decimal CalculateDecimalResult(string operation, decimal firstNum, decimal secondNum)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return firstNum + secondNum;
+                case "-":
+                    return firstNum - secondNum;
+                case "*":
+                    return firstNum * secondNum;
+                case "/":
+                    return firstNum / secondNum;
+                default:
+                    return -1;
+            }
+        }
+
+    }
+}
