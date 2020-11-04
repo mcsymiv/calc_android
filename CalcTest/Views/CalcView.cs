388a8d4447386f6c09a736140e3539de98024fa2
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-11-04 10:30:48 +0200

    Update CalcView class

--- CalcTest/Views/CalcView.cs
+++ CalcTest/Views/CalcView.cs
@@ -13,10 +13,23 @@ namespace CalcTest.Views
         public static IApp App => AppInit.App;
         public Query textView = x => x.Id("input");
         public Query btnDel = x => x.Marked("DEL");
+
         public Query btnZero = x => x.Marked("0");
         public Query btnOne = x => x.Marked("1");
         public Query btnTwo = x => x.Marked("2");
+        public Query btnThree = x => x.Marked("3");
+        public Query btnFour = x => x.Marked("4");
+        public Query btnFive = x => x.Marked("5");
+        public Query btnSix = x => x.Marked("6");
+        public Query btnSeven = x => x.Marked("7");
+        public Query btnEight = x => x.Marked("8");
+        public Query btnNine = x => x.Marked("9");
+        public Query btnComma = x => x.Marked(",");
+
         public Query btnPlus = x => x.Marked("+");
+        public Query btnProduct = x => x.Marked("*");
+        public Query btnDivide = x => x.Marked("/");
+        public Query btnMinus = x => x.Marked("-");
         public Query btnEqual = x => x.Marked("=");
         public static void Repl()
         {
@@ -27,14 +40,74 @@ namespace CalcTest.Views
             App.Tap(btnOne);
             return this;
         }
-        public CalcView TapOnPlus()
+        public CalcView TapOnTwo()
         {
-            App.Tap(btnPlus);
+            App.Tap(btnTwo);
+            return this;
+        }
+        public CalcView TapOnThree()
+        {
+            App.Tap(btnThree);
+            return this;
+        }
+        public CalcView TapOnFour()
+        {
+            App.Tap(btnFour);
+            return this;
+        }
+        public CalcView TapOnFive()
+        {
+            App.Tap(btnFive);
+            return this;
+        }
+        public CalcView TapOnSix()
+        {
+            App.Tap(btnSix);
+            return this;
+        }
+        public CalcView TapOnSeven()
+        {
+            App.Tap(btnSeven);
+            return this;
+        }
+        public CalcView TapOnEight()
+        {
+            App.Tap(btnEight);
+            return this;
+        }
+        public CalcView TapOnNine()
+        {
+            App.Tap(btnNine);
+            return this;
+        }
+        public CalcView TapOnComma()
+        {
+            App.Tap(btnComma);
             return this;
         }
         public CalcView TapOnEqual()
         {
             App.Tap(btnEqual);
+            return this;
+        }
+        public CalcView TapOnPlus()
+        {
+            App.Tap(btnPlus);
+            return this;
+        }
+        public CalcView TapOnMinus()
+        {
+            App.Tap(btnMinus);
+            return this;
+        }
+        public CalcView TapOnProduct()
+        {
+            App.Tap(btnProduct);
+            return this;
+        }
+        public CalcView TapOnDivide()
+        {
+            App.Tap(btnDivide);
             return this;
         }
         public string GetResult()
